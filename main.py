ce9720d873543a92fa90bd7bf7f160ee83cc0844
ghubisz <gab.op.pl@op.pl>
2024-01-02 19:50:05 +0100

    first placeholder

--- main.py
+++ main.py
@@ -3,5 +3,21 @@ from tkinter import ttk
 
 root = tk.Tk()
 
+style = ttk.Style(root)
+root.tk.call("source", "forest-light.tcl")
+root.tk.call("source", "forest-dark.tcl")
+style.theme_use("forest dark")
+
+frame = ttk.Frame(root)
+frame.pack()
+
+widgets_frame = ttk.LabelFrame(frame, text="Insert Row")
+widgets_frame.grid(row=0, column=0)
+
+name_entry = ttk.Entry(widgets_frame)
+name_entry.insert(0, "Name")
+name_entry.bind("<FocusIn>", lambda e: name_entry.delete('0', 'end'))
+name_entry.grid(row=0, column=0, sticky="ew")
+
 root.mainloop()
 
